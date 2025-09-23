89451747559002fd58c185d10f339f1a0715dff8
MBE10 <165320368+MBE10@users.noreply.github.com>
2025-09-23 17:13:14 +0200

    new

--- lesson19/main2.py
+++ lesson19/main2.py
@@ -16,4 +16,7 @@ with st.sidebar.form("book_form"):
     new_author = st.text_input("Author Name")
     new_user_rating = st.slider("user rating", 0.5, 5.0, 0.0, 0.1)
     new_reviews = st.number_input("Num Inputs" min_value=0, step=1)
-    
+    new_price = st.number_input("Price", min_value=1, step=1)
+    new_year = st.number_input("Year", min_value=2009, max_value=2025)
+    new_ganre = st.selections("Ganre", books_df['Ganre'].unique())
+    submit_button = st.form_submit_button(label="Add book")
