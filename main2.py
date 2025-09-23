794785ea9527dc0a6c7a4d7769ab2e53c1be7faa
MBE10 <165320368+MBE10@users.noreply.github.com>
2025-09-23 17:02:37 +0200

    Create main2.py

---
+++ main2.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import streamlit as st
+import ploty.express as px
+
+book_df = pd.read_csv('books.csv')
+
+st.title('Bestselling book analysis')
+st.write("This is a simple app for getting to now the best books")
+
+
+#sidebar
+
+st.sidebar.header("add a new book")
+with st.sidebar.form("book_form"):
+    new_name = st.text_input("Book Name")
+    new_author = st.text_input("Author Name")
+    new_user_rating = st.slider("user rating", 0.5, 5.0, 0.0, 0.1)
+    new_reviews = st.number_input("Num Inputs" min_value=0, step=1)
+    
